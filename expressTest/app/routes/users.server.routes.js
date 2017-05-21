31d4d602e2bd65727cd4a1f22ef4ac324a586dc9
Brian Bruce <public1@brianbruce.org>
2017-05-21 14:08:58 -0400

    chapter 5

--- expressTest/app/routes/users.server.routes.js
+++ expressTest/app/routes/users.server.routes.js
@@ -1,5 +1,16 @@
 var users = require('../../app/controllers/users.server.controller');
 
 module.exports = function(app) {
-  app.route('/users').post(users.create);
+  app.route('/users')
+  	// if post, insert a new user
+    .post(users.create)
+    // If get, return a list
+    .get(users.list);
+
+  app.route('/users/:userId')
+    .get(users.read)
+    .put(users.update)
+    .delete(users.delete);  
+
+  app.param('userId', users.userByID);
 };
