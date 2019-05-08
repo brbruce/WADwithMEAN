ef0d37fe917297b5ae9b72abdb222e193a94624f
Brian Bruce <public1@brianbruce.org>
2019-05-08 15:08:01 -0400

    Updated

--- expressTest/config/env/development.js
+++ expressTest/config/env/development.js
@@ -7,8 +7,10 @@ module.exports = {
   // Database
   db: 'mongodb://localhost/mean-book',
   facebook: {
-    clientID: '1833392170311738',
-    clientSecret: '63658dc7b536a8ca57687b07666a8496',
+    // Replace with actual values from LP
+    clientID: '<FB_CLIENT_ID>',
+    // Replace with actual values from LP
+    clientSecret: '<FB_CLIENT_SECRET>',
     callbackURL: 'http://localhost:81/oauth/facebook/callback'
   },
   twitter: {
@@ -17,8 +19,10 @@ module.exports = {
 		callbackURL: 'http://localhost:3000/oauth/twitter/callback'
 	},
   google: {
-    clientID: '979781646736-ajrr0vbha6t1mio2u6ip8hhb60vh3tl9.apps.googleusercontent.com',
-    clientSecret: 'vemzDwVqhgdSIbM1HE1UOIE6',
+    // Replace with actual values from LP
+    clientID: '<GOOGLE_CLIENT_ID>',
+    // Replace with actual values from LP
+    clientSecret: '<GOOGLE_CLIENT_SECRET>',
     callbackURL: 'http://localhost:81/oauth/google/callback'
   }
 };
